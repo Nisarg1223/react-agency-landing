f95b35c4183390614c8f0a07d7582b47d6ee9e41
Nisarg Darji <darjinisarg49@gmail.com>
2026-02-14 23:40:34 +0530

    more animations

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -25,6 +25,12 @@ const Agence = () => {
          end:'top -83%',
          scrub:true,
          pin:true,
+         pinSpacing:true,
+         pinReparent:true,
+         pinType:'transform',
+         scrub:1,
+         anticipatePin:1,
+         invalidateOnRefresh:true,
          onUpdate:function(e){
           let imageIndex;
           if(e.progress < 1){
@@ -41,17 +47,17 @@ const Agence = () => {
  })
 
   return (
-    <div>
-      <div className='section1'>
+    <div className='bg-white'>
+      <div className='section1 relative py-1'>
       <div ref={imageDivRef} className='h-[20vw] overflow-hidden rounded-4xl w-[15vw] absolute top-60 left-[30vw]'>
         <img ref={imageRef} className='h-full w-full object-cover' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
       </div>
       <div className='relative font-[font2]'>
-    <div className='mt-[55vh]'>
+    <div className='mt-[55vh] text-black'>
       <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br/>
 Douze</h1>
     </div>
-    <div className='pl-[40%] mt-20'>
+    <div className='pl-[40%] mt-20  text-black'>
       <p className='text-5xl '>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
     </div>
     </div>
